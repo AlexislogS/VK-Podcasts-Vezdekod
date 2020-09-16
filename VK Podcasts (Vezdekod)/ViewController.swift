99e2ff8231dd95b019143d098601a6bc1f92a56c
AlexislogS <55928329+AlexislogS@users.noreply.github.com>
2020-09-16 12:22:34 +0300

    Initial Commit

---
+++ VK Podcasts (Vezdekod)/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VK Podcasts (Vezdekod)
+//
+//  Created by Alex Yatsenko on 16.09.2020.
+//  Copyright © 2020 AlexislogS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
