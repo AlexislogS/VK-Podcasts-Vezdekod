92f459e1e510428f2f57fcf7c2ca906b712a0c12
AlexislogS <55928329+AlexislogS@users.noreply.github.com>
2020-09-17 12:27:47 +0300

    Added Preview screen

---
+++ VK Podcasts (Vezdekod)/Controllers/PreviewViewController.swift
@@ -0,0 +1,30 @@
+//
+//  PreviewViewController.swift
+//  VK Podcasts (Vezdekod)
+//
+//  Created by Alex Yatsenko on 17.09.2020.
+//  Copyright © 2020 AlexislogS. All rights reserved.
+//
+
+import UIKit
+
+class PreviewViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
